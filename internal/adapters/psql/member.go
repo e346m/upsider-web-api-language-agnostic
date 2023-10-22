bd227bc290008505425f51bd206cf6d3885ce29d
Eiji Mishiro <eiji346g@gmail.com>
2023-10-22 20:42:52 +0900

    create sign in usecase

--- internal/adapters/psql/member.go
+++ internal/adapters/psql/member.go
@@ -6,10 +6,52 @@ import (
 	dbmodel "github.com/e346m/upsider-wala/db/schema"
 	"github.com/e346m/upsider-wala/internal/domains"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 func (p *PSQL) GetMemberByEmail(ctx context.Context, email string) (*domains.Member, error) {
-	return &domains.Member{}, nil
+	ex := p.getExecutor(ctx)
+	row, err := dbmodel.Members(
+		qm.Where("email = ?", email),
+		qm.Load(dbmodel.MemberRels.Organization),
+	).One(ctx, ex)
+	if err != nil {
+		return &domains.Member{}, nil
+	}
+
+	id, err := p.id.BinaryToString(row.ID)
+	if err != nil {
+		return &domains.Member{}, nil
+	}
+
+	dom := &domains.Member{
+		ID:       id,
+		FullName: row.FullName,
+		Email:    row.Email,
+		Password: row.Password,
+	}
+
+	orgRow := row.R.Organization
+	if orgRow == nil {
+		return &domains.Member{}, nil
+	}
+
+	orgId, err := p.id.BinaryToString(row.R.Organization.ID)
+	if err != nil {
+		return &domains.Member{}, nil
+	}
+
+	org := &domains.Organization{
+		ID:              orgId,
+		Name:            orgRow.Name,
+		Rrepresentative: orgRow.Representative,
+		PhoneNumber:     orgRow.PhoneNumber,
+		Address:         orgRow.Address,
+	}
+
+	dom.Organization = org
+
+	return dom, nil
 }
 
 func (p *PSQL) SaveMember(ctx context.Context, dom *domains.Member) error {
--- internal/usecases/sing_in.go
+++ internal/usecases/sing_in.go
@@ -6,7 +6,7 @@ import (
 	"github.com/e346m/upsider-wala/internal/domains"
 )
 
-func (u *Usecase) SignIn(ctx context.Context, email, password string) (token string, err error) {
+func (u *Usecase) SignIn(ctx context.Context, email, password string) (accessToken, refreshToken string, err error) {
 	ctx, span := u.tracer.Start(ctx, "SignIn")
 	defer span.End()
 
@@ -18,8 +18,12 @@ func (u *Usecase) SignIn(ctx context.Context, email, password string) (token str
 		return
 	}
 
-	// add workflow generating jwt later
-	token = "tmp"
+	accessToken, refreshToken, err = u.auther.GenerateToken(ctx, user.ID, user.FullName, user.Organization.ID)
 
-	return token, nil
+	if err != nil {
+		span.RecordError(err)
+		return
+	}
+
+	return
 }
