7502eaa0aca4b76c2a2f40e944cb86dc813922fb
えいじ <eiji346g@gmail.com>
2023-10-23 15:22:36 +0000

    fix comment

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,10 @@
-// Set up global gitignore for direnv.
+#!/bin/bash
+set -ex
+# Set up global gitignore for direnv.
 mkdir -p ~/.config/git && printf '.direnv/\\n.envrc\\n' > ~/.config/git/ignore && git config --global core.excludesfile ~/.config/git/ignore
-// Install git-crypt for codespace hook?
+# Install git-crypt for codespace hook?
 nix profile install nixpkgs#git-crypt
-// Install, set up and allow direnv in workspace.
+# Install, set up and allow direnv in workspace.
 nix profile install nixpkgs#direnv nixpkgs#nix-direnv && mkdir -p ~/.config/direnv && echo 'source $HOME/.nix-profile/share/nix-direnv/direnvrc' >> ~/.config/direnv/direnvrc && cp envrc.recommended .envrc && direnv allow
-// Add shell hook for direnv
+# Add shell hook for direnv
 echo 'eval \"$(direnv hook bash)\"' >> ~/.bashrc
