f8c1dcbd539dbdde631dd2c30f85817b646f82fb
えいじ <eiji346g@gmail.com>
2023-10-23 15:35:54 +0000

    rm backslash

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,4 +7,4 @@ nix profile install nixpkgs#git-crypt
 # Install, set up and allow direnv in workspace.
 nix profile install nixpkgs#direnv nixpkgs#nix-direnv && mkdir -p ~/.config/direnv && echo 'source $HOME/.nix-profile/share/nix-direnv/direnvrc' >> ~/.config/direnv/direnvrc && cp envrc.recommended .envrc && direnv allow
 # Add shell hook for direnv
-echo 'eval \"$(direnv hook bash)\"' >> ~/.bashrc
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
